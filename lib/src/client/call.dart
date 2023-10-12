92875651fc8e65f2b2b36ef7709d965efe60933c
MohsinJaved <m.mohsinjaved2010@gmail.com>
2023-10-12 14:55:23 +0500

    added check

--- lib/src/client/call.dart
+++ lib/src/client/call.dart
@@ -429,6 +429,9 @@ class ClientCall<Q, R> implements Response {
     //   /// Process status error if necessary
     //   _checkForErrorStatus(_headerMetadata);
     // }
+    if (!_hasReceivedResponses) {
+      _checkForErrorStatus(_headerMetadata);
+    }
     _responseTimeline?.finish();
     _timeoutTimer?.cancel();
     _responses.close();
